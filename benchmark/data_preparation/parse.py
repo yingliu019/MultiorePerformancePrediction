19c415914ed07f190bd0808882919669e551ce67
yingliu <yingliu019@gmail.com>
2024-04-01 00:42:42 -0700

    rodina

--- benchmark/data_preparation/parse.py
+++
@@ -1,6 +0,0 @@
-import subprocess 
-try: 
-    ans = subprocess.check_output(["python", "--version"], text=True) 
-    print(type(ans), ans) 
-except subprocess.CalledProcessError as e: 
-    print(f"Command failed with return code {e.returncode}")
